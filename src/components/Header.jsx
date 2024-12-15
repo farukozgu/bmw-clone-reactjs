8a8da089198efdbb5f2492d790e5559290f32425
farukozgu <faruk.ozgufb@gmail.com>
2024-12-15 15:01:58 +0300

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header">
+        <div className="logo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg"
+            alt="BMW Logo"
+          />
+        </div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="#modeller">Modeller</a>
+            </li>
+            <li>
+              <a href="#elektrikli-modeller">%100 Elektrikli Modeller</a>
+            </li>
+            <li>
+              <a href="#online-rezervasyon">BMW'nizi Online Rezerve Edin</a>
+            </li>
+            <li>
+              <a href="#online-hizmetler">Online Hizmetler</a>
+            </li>
+            <li>
+              <a href="#bmw-kesfet">BMW'yi Keşfedin</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
