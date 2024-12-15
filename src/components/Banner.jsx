602e2f7c4ad856cea2728687ce0b64c06552ecd3
muratcansalihW <muratcansalihwork@hotmail.com>
2024-12-16 01:22:54 +0300

    test

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,6 @@ function Banner() {
       <video className="videoTag" autoPlay loop muted>
         <source src={video} type="video/mp4" />
       </video>
-      
       <div className="container">
         <div className="row bannerRow">
           <div className="col-md-12 bannerTexts">
