a48b7cd302e4d95cd97a957a35cd7ce668d48918
muratcansalihW <muratcansalihwork@hotmail.com>
2024-12-15 15:09:04 +0300

    banner-update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="container">
+      <div className="row bannerRow">
+        <div className="col-md-12 bannerTexts">
+          <h1 className="bannerTitle">
+            BMW RUHU: HER RUHA
+            <br />
+            BİR RUH EŞİ.
+          </h1>
+          <p className="bannerDsc">BMW modellerinde öze yıl sonu fırsatları.</p>
+          <button className="inceleBtn">İnceleyin</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
