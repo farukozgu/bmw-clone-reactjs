5358ef877ed4fc8e1f56ebf663b0326f4a370553
farukozgu <faruk.ozgufb@gmail.com>
2024-12-15 15:10:40 +0300

    header logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,34 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <>
       <header className="header">
-        <div className="logo">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg"
-            alt="BMW Logo"
-          />
+        <div className="container">
+          <div className="logo">
+            <img src="../assets/logo.svg" alt="BMW Logo" />
+          </div>
+          <nav className="nav">
+            <ul>
+              <li>
+                <a href="#modeller">Modeller</a>
+              </li>
+              <li>
+                <a href="#elektrikli-modeller">%100 Elektrikli Modeller</a>
+              </li>
+              <li>
+                <a href="#online-rezervasyon">BMW'nizi Online Rezerve Edin</a>
+              </li>
+              <li>
+                <a href="#online-hizmetler">Online Hizmetler</a>
+              </li>
+              <li>
+                <a href="#bmw-kesfet">BMW'yi Keşfedin</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <a href="#modeller">Modeller</a>
-            </li>
-            <li>
-              <a href="#elektrikli-modeller">%100 Elektrikli Modeller</a>
-            </li>
-            <li>
-              <a href="#online-rezervasyon">BMW'nizi Online Rezerve Edin</a>
-            </li>
-            <li>
-              <a href="#online-hizmetler">Online Hizmetler</a>
-            </li>
-            <li>
-              <a href="#bmw-kesfet">BMW'yi Keşfedin</a>
-            </li>
-          </ul>
-        </nav>
       </header>
     </>
   );
