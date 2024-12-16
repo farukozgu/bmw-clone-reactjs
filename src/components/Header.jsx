37f86f37dd2655f1d79429753ad4720ef9e27cb7
farukozgu <faruk.ozgufb@gmail.com>
2024-12-16 13:55:35 +0300

    header nav menu updated.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,29 +30,34 @@ function Header() {
       <header className={`header ${isOpen ? "menu-open" : ""}`}>
         <div className="container">
           <div className={`navbar ${isOpen ? "navbar-open" : ""}`}>
-            <div className="logo">
+            {/* <div className="logo">
               <img src={isOpen ? mobileLogo : logo} alt="BMW Logo" />{" "}
+            </div> */}
+            <div className="parentNav flex items-center">
+              <img src={isOpen ? mobileLogo : logo} alt="BMW Logo" />{" "}
+              <nav className="desktopNav nav ps-4">
+                <ul>
+                  <li>
+                    <a href="#modeller">Modeller</a>
+                  </li>
+                  <li>
+                    <a href="#elektrikli-modeller">%100 Elektrikli Modeller</a>
+                  </li>
+                  <li>
+                    <a href="#online-rezervasyon">
+                      BMW'nizi Online Rezerve Edin
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#online-hizmetler">Online Hizmetler</a>
+                  </li>
+                  <li>
+                    <a href="#bmw-kesfet">BMW'yi Keşfedin</a>
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <nav className="desktopNav nav ps-4">
-              <ul>
-                <li>
-                  <a href="#modeller">Modeller</a>
-                </li>
-                <li>
-                  <a href="#elektrikli-modeller">%100 Elektrikli Modeller</a>
-                </li>
-                <li>
-                  <a href="#online-rezervasyon">BMW'nizi Online Rezerve Edin</a>
-                </li>
-                <li>
-                  <a href="#online-hizmetler">Online Hizmetler</a>
-                </li>
-                <li>
-                  <a href="#bmw-kesfet">BMW'yi Keşfedin</a>
-                </li>
-              </ul>
-            </nav>
-            <div className="float-end">
+            <div className="float-right">
               <a className="location-a" href="">
                 <img src={location} alt="location" />
               </a>
