57c3f6d944adcecabce9d0da091534458d15e677
muratcansalihW <muratcansalihwork@hotmail.com>
2024-12-17 00:39:12 +0300

    footerdesktopcompleted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import "./Footer.css";
 import React from "react";
+import Youtube from "../assets/youtube.png";
+import X from "../assets/x.png";
+import Linkedin from "../assets/linkedin.png";
+import Instagram from "../assets/instagram.png";
+import Facebook from "../assets/facebook.png";
 
 export default function Footer() {
   return (
     <>
-      <div className="container">
-        <div className="row">
+      <div className="container-fluid containerBorder">
+        <div className="row firstRow">
           <div className="col-md-12 footerLineFirst">
             <p className="lineFirstText">BMW Modelleri'ni Keşfedin</p>
             <a href="/" className="lnkFirstLine">
@@ -84,6 +89,36 @@ export default function Footer() {
               </a>
               <a className="lnkBlock" href="/">
                 Borusan Otomotiv
+              </a>
+            </div>
+          </div>
+          <div className="row footerIcon">
+            <img src={Youtube} className="ftIcon" />
+            <img src={Facebook} className="ftIcon" />
+            <img src={Instagram} className="ftIcon" />
+            <img src={X} className="ftIcon" />
+            <img src={Linkedin} className="ftIcon" />
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid footerContainerLast">
+        <div className="footerParentLast">
+          <div className="row">
+            <div className="col-md-6 lastLeft">
+              <p className="bmwCode">© BMW AG 2024</p>
+            </div>
+            <div className="col-md-6 lastRight">
+              <a className="lastBlank" href="">
+                Yakıt Ekonomisi ve Emisyon Bilgileri
+              </a>
+              <a className="lastBlank" href="">
+                Tanımlama Bilgileri (Çerezler)
+              </a>
+              <a className="lastBlank" href="">
+                Yasal Notlar
+              </a>
+              <a className="lastBlank" href="">
+                Kişisel Verilerin Korunması
               </a>
             </div>
           </div>
