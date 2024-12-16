f82b095bfef3a286ce945b80814661c6e6216b09
muratcansalihW <muratcansalihwork@hotmail.com>
2024-12-16 13:20:12 +0300

    test

--- src/components/Reserved.jsx
+++ src/components/Reserved.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Reserved.css";
 
-export default function Reserved({image, title, button}) {
+export default function Reserved({ image, title, button }) {
   return (
     <section>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12 cardBody">
-                        <img src={image}/>
-                        <p>{title}</p>
-                        <button onClick={onButtonClick}>{buttonText}</button>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 cardBody">
+            <div className="cardsDetail">
+              <img src={image} />
+              <p>{title}</p>
+              <button>{button}</button>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
-
