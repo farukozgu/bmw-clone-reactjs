b0db65f7abddb444b30156e8952559293a16f4d5
muratcansalihW <muratcansalihwork@hotmail.com>
2024-12-16 14:03:42 +0300

    tewst

--- src/components/Reserved.jsx
+++ src/components/Reserved.jsx
@@ -3,16 +3,14 @@ import "./Reserved.css";
 
 export default function Reserved({ image, title, button }) {
   return (
-    <section>
-      <div className="row">
-        <div className="col-md-12 cardBody">
-          <div className="cardsDetail">
-            <img className="iconCard" src={image} />
-            <p className="detailP">{title}</p>
-            <button className="cardBtn">{button}</button>
-          </div>
+    <div className="row">
+      <div className="col-md-12 cardBody">
+        <div className="cardsDetail">
+          <img className="iconCard" src={image} />
+          <p className="detailP">{title}</p>
+          <button className="cardBtn">{button}</button>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
